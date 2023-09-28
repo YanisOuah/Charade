18d0f75564924728f7ac23732e00c184a9ff956c
Yanis Ouahab <yanisouah@gmail.com>
2023-09-28 16:16:26 +0200

    added navigation

--- android/app/src/main/java/com/charade/MainActivity.java
+++ android/app/src/main/java/com/charade/MainActivity.java
@@ -1,4 +1,5 @@
 package com.charade;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -28,5 +29,11 @@ public class MainActivity extends ReactActivity {
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
+
+  //https://reactnavigation.org/docs/getting-started/ just copied this
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
---
+++ src/screens/GameScreen/GameScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const GameScreen = () => {
+  return (
+    <View>
+      <Text>Main Screen</Text>
+    </View>
+  );
+};
+export default GameScreen;
